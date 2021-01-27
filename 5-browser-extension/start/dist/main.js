c1cfaf02e4558efb3ab950e58987bff538d0736f
Shawn Thai <shawnt@sfu.ca>
2021-01-26 19:50:42 -0800

    [JS] CO2Signal api function call

--- 5-browser-extension/start/dist/main.js
+++ 5-browser-extension/start/dist/main.js
@@ -1,3 +1,5 @@
+import axios from '../node_modules/axios';
+
 // form fields
 const form = document.querySelector('.form-data');
 const region = document.querySelector('.region-name');
@@ -11,6 +13,41 @@ const usage = document.querySelector('.carbon-usage');
 const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
+
+// function call to CO2Signal API
+async function displayCarbonUsage(apiKey, region) {
+	try {
+		await axios
+			.get('https://api.co2signal.com/v1/latest', {
+				params: {
+					countryCode: region,
+				},
+				headers: {
+					'auth-token': apiKey,
+				},
+			})
+			.then((response) => {
+				let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+				//calculateColor(CO2);
+
+				loading.style.display = 'none';
+				form.style.display = 'none';
+				myregion.textContent = region;
+				usage.textContent =
+					Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+				fossilfuel.textContent =
+					response.data.data.fossilFuelPercentage.toFixed(2) +
+					'% (percentage of fossil fuels used to generate electricity)';
+				results.style.display = 'block';
+			});
+	} catch (error) {
+		console.log(error);
+		loading.style.display = 'none';
+		results.style.display = 'none';
+		errors.textContent = 'Sorry, we have no data for the region you have requested.';
+	}
+}
 
 // Initializes the extension
 function init() {
@@ -37,7 +74,7 @@ function init() {
 	}
 };
 
-// Rests local storage
+// Resets local storage
 function reset(e) {
 	e.preventDefault();
 	//clear local storage for region only
