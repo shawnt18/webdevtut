4fc9815934d2f771100e7db997d1a9bcdfb664fb
Shawn Thai <shawnt@sfu.ca>
2021-01-28 22:24:05 -0800

    [Part 3] add GameObject class

--- 6-space-game/2-drawing-to-canvas/your-work/app.js
+++ 6-space-game/2-drawing-to-canvas/your-work/app.js
@@ -1,3 +1,19 @@
+class GameObject {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.dead = false;
+		this.type = "";
+		this.width = 0;
+		this.height = 0;
+		this.img = undefined;
+	}
+
+	draw(ctx) {
+		ctx.drawImage(this.img, this.x, this,y, this.width, this.height);
+	}
+}
+
 function loadTexture(path) {
 	return new Promise((resolve) => {
 		const img = new Image();
