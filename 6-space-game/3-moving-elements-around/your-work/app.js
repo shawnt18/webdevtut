64290486e1320819e6a8255fc5b05bd1fc204daa
Shawn Thai <shawnt@sfu.ca>
2021-01-28 23:16:22 -0800

    Copy written code to Part 3 folder

--- 6-space-game/3-moving-elements-around/your-work/app.js
+++ 6-space-game/3-moving-elements-around/your-work/app.js
@@ -1,38 +1,154 @@
-function loadTexture(path) {
-  return new Promise((resolve) => {
-    const img = new Image();
-    img.src = path;
-    img.onload = () => {
-      resolve(img);
-    };
-  });
+//////////////////
+// GAME OBJECTS //
+class GameObject {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.dead = false;
+		this.type = "";
+		this.width = 0;
+		this.height = 0;
+		this.img = undefined;
+	}
+
+	draw(ctx) {
+		ctx.drawImage(this.img, this.x, this,y, this.width, this.height);
+	}
+}
+
+class Hero extends GameObject {
+	constructor(x, y) {
+		super(x, y);
+		(this.width = 99), (this.height = 75);
+		this.type = 'Hero';
+		this.speed = { x: 0, y: 0 };
+	}
+}
+
+class Enemy extends GameObject {
+	constructor(x, y) {
+		super(x, y);
+		(this.width = 98), (this.height = 50);
+		this.type = 'Enemy';
+		let id = setInterval(() => {
+			if (this.y < canvas.height - this.height) {
+				this.y += 5;
+			} else {
+				console.log('Stopped at', this.y);
+				clearInterval(id);
+			}
+		}, 300);
+	}
 }
 
 function createEnemies(ctx, canvas, enemyImg) {
-  const MONSTER_TOTAL = 5;
-  const MONSTER_WIDTH = MONSTER_TOTAL * 98;
-  const START_X = (canvas.width - MONSTER_WIDTH) / 2;
-  const STOP_X = START_X + MONSTER_WIDTH;
+	const MONSTER_TOTAL = 5;
+	const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+	const START_X = (canvas.width - MONSTER_WIDTH) / 2;
+	const STOP_X = START_X + MONSTER_WIDTH;
 
-  for (let x = START_X; x < STOP_X; x += 98) {
-    for (let y = 0; y < 50 * 5; y += 50) {
-      ctx.drawImage(enemyImg, x, y);
-    }
-  }
+	for (let x = START_X; x < STOP_X; x += 98) {
+		for (let y = 0; y < 50 * 5; y += 50) {
+			ctx.drawImage(enemyImg, x, y);
+		}
+	}
+};
+
+////////////
+// EVENTS //
+class EventEmitter {
+	constructor() {
+		this.listeners = {};
+	}
+
+	on(message, listener) {
+		if (!this.listeners[message]) {
+			this.listeners[message] = [];
+		}
+		this.listeners[message].push(listener);
+	}
+
+	emit(message, payload = null) {
+		if (this.listeners[message]) {
+			this.listeners[message].forEach((l) => l(message, payload));
+		}
+	}
+}
+
+const Messages = {
+	KEY_EVENT_UP: "KEY_EVENT_UP",
+	KEY_EVENT_DOWN: "KEY_EVENT_DOWN",
+	KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
+	KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
+};
+
+let heroImg,
+	enemyImg,
+	laserImg,
+	canvas, ctx,
+	gameObjects = [],
+	hero,
+	eventEmitter = new EventEmitter();
+
+let onKeyDown = function (e) {
+	console.log(e.keyCode);
+	switch (e.keyCode) {
+		case 37:
+		case 39:
+		case 38:
+		case 40: // Arrow keys
+		case 32:
+			e.preventDefault();
+			break; // Space
+		default:
+			break; // do not block other keys
+	  }
+};
+
+window.addEventListener("keydown", onKeyDown);
+
+window.addEventListener("keyup", (evt) => {
+	if (evt.key === "ArrowUp") {
+		eventEmitter.emit(Messages.KEY_EVENT_UP);
+	} else if (evt.key === "ArrowDown") {
+		eventEmitter.emit(Messages.KEY_EVENT_DOWN);
+	} else if (evt.key === "ArrowLeft") {
+		eventEmitter.emit(Messages.KEY_EVENT_LEFT);
+	} else if (evt.key === "ArrowRight") {
+		eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
+	}
+});
+
+///////////////
+// GAME LOOP //
+function loadTexture(path) {
+	return new Promise((resolve) => {
+		const img = new Image();
+		img.src = path;
+		img.onload = () => {
+			resolve(img);
+		};
+	});
 }
 
 window.onload = async () => {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  const heroImg = await loadTexture("assets/player.png");
-  const enemyImg = await loadTexture("assets/enemyShip.png");
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
 
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(
-    heroImg,
-    canvas.width / 2 - 45,
-    canvas.height - canvas.height / 4
-  );
-  createEnemies(ctx, canvas, enemyImg);
+	// load textures
+	const heroImg = await loadTexture('assets/player.png');
+	const enemyImg = await loadTexture('assets/enemyShip.png');
+
+	// draw black background
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0,0, 1024, 768); // x,y,width,height
+
+	// draw hero
+	ctx.drawImage(
+		heroImg,
+		canvas.width / 2 - 45, 
+		canvas.height - canvas.height / 4);
+
+	// draw enemies
+	createEnemies(ctx, canvas, enemyImg);
 };
