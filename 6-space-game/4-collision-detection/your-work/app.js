929a782b83e6a3ca396bf21fc1526d4e44ab315c
Shawn Thai <shawnt@sfu.ca>
2021-01-29 14:34:30 -0800

    [Part 4] Laser class

--- 6-space-game/4-collision-detection/your-work/app.js
+++ 6-space-game/4-collision-detection/your-work/app.js
@@ -59,6 +59,23 @@ class Enemy extends GameObject {
 	}
 }
 
+class Laser extends GameObject {
+	constructor(x, y) {
+		super(x, y);
+		(this.width = 9), (this.height = 33);
+		this.type = 'Laser';
+		this.img = laserImg;
+		let id = setInterval(() => {
+			if (this.y > 0) {
+				this.y -= 15;
+			} else {
+				this.dead = true;
+				clearInterval(id);
+			}
+		}, 100)
+	}
+}
+
 ////////////
 // EVENTS //
 class EventEmitter {
