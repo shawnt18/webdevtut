6d38f9f30238465f185ecf160314f8115331ec69
Shawn Thai <shawnt@sfu.ca>
2021-01-28 22:06:35 -0800

    [Part 2] draw player and enemies

--- 6-space-game/2-drawing-to-canvas/your-work/app.js
+++ 6-space-game/2-drawing-to-canvas/your-work/app.js
@@ -9,8 +9,17 @@ function loadTexture(path) {
 }
 
 function createEnemies(ctx, canvas, enemyImg) {
-	// TODO draw enemies
-}
+	const MONSTER_TOTAL = 5;
+	const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+	const START_X = (canvas.width - MONSTER_WIDTH) / 2;
+	const STOP_X = START_X + MONSTER_WIDTH;
+
+	for (let x = START_X; x < STOP_X; x += 98) {
+		for (let y = 0; y < 50 * 5; y += 50) {
+			ctx.drawImage(enemyImg, x, y);
+		}
+	}
+};
 
 window.onload = async () => {
 	canvas = document.getElementById('canvas');
@@ -18,15 +27,18 @@ window.onload = async () => {
 
 	// load textures
 	const playerImg = await loadTexture('assets/player.png');
-	const enemmyImg = await loadTexture('assets/enemyShip.png');
+	const enemyImg = await loadTexture('assets/enemyShip.png');
 
 	// draw black background
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0,0, 1024, 768); // x,y,width,height
 
 	// draw hero
-	
+	ctx.drawImage(
+		playerImg, 
+		canvas.width / 2 - 45, 
+		canvas.height - canvas.height / 4);
 
-	// TODO uncomment the next line when you add enemies to screen
-	//createEnemies(ctx, canvas, enemyImg);
+	// draw enemies
+	createEnemies(ctx, canvas, enemyImg);
 };
