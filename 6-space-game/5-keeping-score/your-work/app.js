2d768c24770564481a6f8a4da47160eb8a6ebb71
Shawn Thai <shawnt@sfu.ca>
2021-01-29 18:52:23 -0800

    Copy written code & assets to Part 5 folder

--- 6-space-game/5-keeping-score/your-work/app.js
+++ 6-space-game/5-keeping-score/your-work/app.js
@@ -1,29 +1,11 @@
-// @ts-check
-class EventEmitter {
-	constructor() {
-		this.listeners = {};
-	}
-
-	on(message, listener) {
-		if (!this.listeners[message]) {
-			this.listeners[message] = [];
-		}
-		this.listeners[message].push(listener);
-	}
-
-	emit(message, payload = null) {
-		if (this.listeners[message]) {
-			this.listeners[message].forEach((l) => l(message, payload));
-		}
-	}
-}
-
+//////////////////
+// GAME OBJECTS //
 class GameObject {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 		this.dead = false;
-		this.type = '';
+		this.type = "";
 		this.width = 0;
 		this.height = 0;
 		this.img = undefined;
@@ -38,8 +20,34 @@ class GameObject {
 			top: this.y,
 			left: this.x,
 			bottom: this.y + this.height,
-			right: this.x + this.width,
+			right: this.x + this.width
 		};
+	}
+}
+
+function intersectRect(r1, r2) {
+	return !(
+		r2.left > r1.right ||
+		r2.right < r1.left ||
+		r2.top > r1.bottom ||
+		r2.bottom < r1.top
+	);
+}
+
+class Laser extends GameObject {
+	constructor(x, y) {
+		super(x, y);
+		(this.width = 9), (this.height = 33);
+		this.type = 'Laser';
+		this.img = laserImg;
+		let id = setInterval(() => {
+			if (this.y > 0) {
+				this.y -= 15;
+			} else {
+				this.dead = true;
+				clearInterval(id);
+			}
+		}, 100)
 	}
 }
 
@@ -51,10 +59,10 @@ class Hero extends GameObject {
 		this.speed = { x: 0, y: 0 };
 		this.cooldown = 0;
 	}
+	
 	fire() {
 		gameObjects.push(new Laser(this.x + 45, this.y - 10));
 		this.cooldown = 500;
-
 		let id = setInterval(() => {
 			if (this.cooldown > 0) {
 				this.cooldown -= 100;
@@ -63,6 +71,7 @@ class Hero extends GameObject {
 			}
 		}, 200);
 	}
+
 	canFire() {
 		return this.cooldown === 0;
 	}
@@ -84,58 +93,50 @@ class Enemy extends GameObject {
 	}
 }
 
-class Laser extends GameObject {
-	constructor(x, y) {
-		super(x, y);
-		(this.width = 9), (this.height = 33);
-		this.type = 'Laser';
-		this.img = laserImg;
-		let id = setInterval(() => {
-			if (this.y > 0) {
-				this.y -= 15;
-			} else {
-				this.dead = true;
-				clearInterval(id);
-			}
-		}, 100);
+////////////
+// EVENTS //
+class EventEmitter {
+	constructor() {
+		this.listeners = {};
+	}
+
+	on(message, listener) {
+		if (!this.listeners[message]) {
+			this.listeners[message] = [];
+		}
+		this.listeners[message].push(listener);
+	}
+
+	emit(message, payload = null) {
+		if (this.listeners[message]) {
+			this.listeners[message].forEach((l) => l(message, payload));
+		}
 	}
 }
 
-function loadTexture(path) {
-	return new Promise((resolve) => {
-		const img = new Image();
-		img.src = path;
-		img.onload = () => {
-			resolve(img);
-		};
-	});
-}
-
-function intersectRect(r1, r2) {
-	return !(r2.left > r1.right || r2.right < r1.left || r2.top > r1.bottom || r2.bottom < r1.top);
-}
-
 const Messages = {
-	KEY_EVENT_UP: 'KEY_EVENT_UP',
-	KEY_EVENT_DOWN: 'KEY_EVENT_DOWN',
-	KEY_EVENT_LEFT: 'KEY_EVENT_LEFT',
-	KEY_EVENT_RIGHT: 'KEY_EVENT_RIGHT',
-	KEY_EVENT_SPACE: 'KEY_EVENT_SPACE',
-	COLLISION_ENEMY_LASER: 'COLLISION_ENEMY_LASER',
+	KEY_EVENT_UP: "KEY_EVENT_UP",
+	KEY_EVENT_DOWN: "KEY_EVENT_DOWN",
+	KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
+	KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
+	KEY_EVENT_SPACE: "KEY_EVENT_SPACE",
+	COLLISION_ENEMY_LASER: "COLLISION_ENEMY_LASER",
+	COLLISION_ENEMY_HERO: "COLLISION_ENEMY_HERO",
+	ENEMY_KILL: "ENEMY_KILL",
 };
 
+// Global objects
 let heroImg,
 	enemyImg,
 	laserImg,
-	canvas,
+	canvas, 
 	ctx,
 	gameObjects = [],
 	hero,
 	eventEmitter = new EventEmitter();
 
-// EVENTS
 let onKeyDown = function (e) {
-	// console.log(e.keyCode);
+	console.log(e.keyCode);
 	switch (e.keyCode) {
 		case 37:
 		case 39:
@@ -146,25 +147,45 @@ let onKeyDown = function (e) {
 			break; // Space
 		default:
 			break; // do not block other keys
-	}
+	  }
 };
 
-window.addEventListener('keydown', onKeyDown);
+window.addEventListener("keydown", onKeyDown);
 
-// TODO make message driven
-window.addEventListener('keyup', (evt) => {
-	if (evt.key === 'ArrowUp') {
+window.addEventListener("keyup", (evt) => {
+	if (evt.key === "ArrowUp") {
 		eventEmitter.emit(Messages.KEY_EVENT_UP);
-	} else if (evt.key === 'ArrowDown') {
+	} else if (evt.key === "ArrowDown") {
 		eventEmitter.emit(Messages.KEY_EVENT_DOWN);
-	} else if (evt.key === 'ArrowLeft') {
+	} else if (evt.key === "ArrowLeft") {
 		eventEmitter.emit(Messages.KEY_EVENT_LEFT);
-	} else if (evt.key === 'ArrowRight') {
+	} else if (evt.key === "ArrowRight") {
 		eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
 	} else if (evt.keyCode === 32) {
 		eventEmitter.emit(Messages.KEY_EVENT_SPACE);
 	}
 });
+
+///////////////
+// GAME LOOP //
+function loadTexture(path) {
+	return new Promise((resolve) => {
+		const img = new Image();
+		img.src = path;
+		img.onload = () => {
+			resolve(img);
+		};
+	});
+}
+
+function createHero() {
+	hero = new Hero(
+		canvas.width / 2 - 45,
+		canvas.height - canvas.height / 4
+	);
+	hero.img = heroImg;
+	gameObjects.push(hero);
+}
 
 function createEnemies() {
 	const MONSTER_TOTAL = 5;
@@ -179,82 +200,87 @@ function createEnemies() {
 			gameObjects.push(enemy);
 		}
 	}
-}
-
-function createHero() {
-	hero = new Hero(canvas.width / 2 - 45, canvas.height - canvas.height / 4);
-	hero.img = heroImg;
-	gameObjects.push(hero);
-}
-
-function updateGameObjects() {
-	const enemies = gameObjects.filter((go) => go.type === 'Enemy');
-	const lasers = gameObjects.filter((go) => go.type === 'Laser');
-	// laser hit something
-	lasers.forEach((l) => {
-		enemies.forEach((m) => {
-			if (intersectRect(l.rectFromGameObject(), m.rectFromGameObject())) {
-				eventEmitter.emit(Messages.COLLISION_ENEMY_LASER, {
-					first: l,
-					second: m,
-				});
-			}
-		});
-	});
-
-	gameObjects = gameObjects.filter((go) => !go.dead);
-}
-
-function drawGameObjects(ctx) {
-	gameObjects.forEach((go) => go.draw(ctx));
-}
+};
 
 function initGame() {
 	gameObjects = [];
-	createEnemies();
 	createHero();
+	createEnemies();
 
 	eventEmitter.on(Messages.KEY_EVENT_UP, () => {
 		hero.y -= 5;
 	});
-
 	eventEmitter.on(Messages.KEY_EVENT_DOWN, () => {
 		hero.y += 5;
 	});
-
 	eventEmitter.on(Messages.KEY_EVENT_LEFT, () => {
 		hero.x -= 5;
 	});
-
 	eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
 		hero.x += 5;
 	});
-
 	eventEmitter.on(Messages.KEY_EVENT_SPACE, () => {
 		if (hero.canFire()) {
 			hero.fire();
 		}
-		// console.log('cant fire - cooling down')
 	});
-
 	eventEmitter.on(Messages.COLLISION_ENEMY_LASER, (_, { first, second }) => {
-		first.dead = true;
-		second.dead = true;
+		if (!first.dead) {
+			first.dead = true;
+			// second.dead = true;
+			second.type = 'Explosion';
+			second.img = redExplosionImg;
+			second.x += (second.width - 56) / 2;
+			second.y += (second.height - 54) / 2;
+			second.width = 56;
+			second.height = 54;
+		}
 	});
+	eventEmitter.on(Messages.ENEMY_KILL, (_, {first} ) => {
+		first.dead = true;
+	});
+}
+
+function drawGameObjects(ctx) {
+	gameObjects.forEach(go => go.draw(ctx));
+}
+
+function updateGameObjects() {
+	const explosions = gameObjects.filter(go => go.type === 'Explosion');
+	explosions.forEach((l) => {
+		eventEmitter.emit(Messages.ENEMY_KILL, {first: l});
+	})
+	const enemies = gameObjects.filter(go => go.type === 'Enemy');
+	const lasers = gameObjects.filter(go => go.type === 'Laser');
+	lasers.forEach((l) => {
+		enemies.forEach((m) => {
+			if (intersectRect(l.rectFromGameObject(), m.rectFromGameObject())) {
+				eventEmitter.emit(
+					Messages.COLLISION_ENEMY_LASER, { first: l, second: m }
+				);
+			}
+		});
+	});
+	gameObjects = gameObjects.filter(go => !go.dead);
 }
 
 window.onload = async () => {
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
+
+	// load textures
+	starBackgroundImg = await loadTexture('assets/starBackground.png');
 	heroImg = await loadTexture('assets/player.png');
 	enemyImg = await loadTexture('assets/enemyShip.png');
 	laserImg = await loadTexture('assets/laserRed.png');
+	redExplosionImg = await loadTexture('assets/laserRedShot.png');
+	let pat = ctx.createPattern(starBackgroundImg, 'repeat');
 
 	initGame();
 	let gameLoopId = setInterval(() => {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.fillStyle = 'black';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0,0, canvas.width, canvas.height); // x,y,width,height
+		ctx.fillStyle = pat;
+		ctx.fillRect(0,0, canvas.width, canvas.height);
 		updateGameObjects();
 		drawGameObjects(ctx);
 	}, 100);
