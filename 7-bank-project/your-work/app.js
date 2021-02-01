4b2d957a1bc4d6b105ce4fd5793f9217628c79ec
Shawn Thai <shawnt@sfu.ca>
2021-01-31 23:51:39 -0800

    [Part 7] template loading function

---
+++ 7-bank-project/your-work/app.js
@@ -0,0 +1,9 @@
+function updateRoute(templateId) {
+  const template = document.getElementById(templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById('app');
+  app.innerHTML = '';
+  app.appendChild(view);
+}
+
+updateRoute('login');
