286f8632ed48e6412f1eef183ec20b249781c720
Shawn Thai <shawnt@sfu.ca>
2021-02-01 00:01:40 -0800

    [Part 7] navigation by updating current URL w/o reloading html

--- 7-bank-project/your-work/app.js
+++ 7-bank-project/your-work/app.js
@@ -3,9 +3,19 @@ const routes = {
 	'/dashboard': { templateId: 'dashboard' },
 };
 
+function navigate(path) {
+	window.history.pushState({}, path, window.location.origin + path);
+	updateRoute();
+}
+
 function updateRoute() {
 	const path = window.location.pathname;
+	console.log(path);
 	const route = routes[path];
+
+	if (!route) {
+		return navigate('/login');
+	}	
 
 	const template = document.getElementById(route.templateId);
 	const view = template.content.cloneNode(true);
@@ -14,4 +24,3 @@ function updateRoute() {
 	app.appendChild(view);
 }
 
-updateRoute();
